c2280c9ac5914d6d28925c8406b65af169977c89
Christian <146226859+KwaziiMedia@users.noreply.github.com>
2024-01-28 23:12:54 -0500

    Create index.js
    added what ill be doing and variables for DOM

---
+++ index.js
@@ -0,0 +1,12 @@
+//variables for elements
+const noBtn = document.getElementbyid("no");
+const yesBtn = document.getElementbyid("yes");
+const dynamicImg = document.getElementbyId("image")
+//functionality
+
+//noBtn function
+
+//yesBtn function
+
+//changing noBtn location function
+
