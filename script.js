a098e329c23d904760f71fc6868df9deee6c0cf1
Christian <christianaguilar2223@gmail.com>
2024-02-10 16:48:10 -0500

    finshed product vs1.1

---
+++ script.js
@@ -0,0 +1,21 @@
+const yesBtn = document.getElementById("yes-btn");
+const noBtn = document.getElementById("no-btn")
+
+function changeImage () {  
+    const image = document.getElementById("image");
+    image.src = "images/love-you-mochi.gif"
+    // noBtn.innerText = "No Take Backs HEHE"
+}
+
+noBtn.addEventListener("mouseover", function (){
+    noBtn.style.left = `${Math.ceil(Math.random() * 50)}%`;
+    noBtn.style.top = `${Math.ceil(Math.random() * 50)}%`;
+    const image = document.getElementById("image");
+    image.src = "/images/sad-cat.gif"
+    noBtn.innerHTML = "PLEASE POOKIE"
+})
+function changeNo () {
+    noBtn.innerHTML = "You Don't Mean That"
+    const image = document.getElementById("image");
+    image.src = "/images/crying.gif"
+}
